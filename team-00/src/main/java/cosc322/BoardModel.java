a558130e8579a897c26ea5a7aff7c273612b914e
Julian Rey <julianandreasrey@gmail.com>
2019-03-01 14:00:25 -0800

    successfully replicated the basic offline funtionality of Gao's code

--- team-00/src/main/java/cosc322/BoardModel.java
+++ team-00/src/main/java/cosc322/BoardModel.java
@@ -14,21 +14,52 @@ public class BoardModel extends GameModel {
     
     private String[][] gameBoard = null;
     
-    public int getRowCount() {
+    public int getSize() {
         return gameBoard.length;
     }
     
-    public int getColumnCount() {
-        return gameBoard[0].length;
+    public String getTile(int row, int column) {
+        return gameBoard[row][column];
     }
     
-    public BoardModel(int rows, int columns) {
-        gameBoard = new String[rows][columns];
+    public void setTile(int row, int column, String occupant) {
+        gameBoard[row][column] = occupant;
+    }
+    
+    // set a white or black queen at the specified tile (used for initialization)
+    public void setQueen(int row, int column, boolean isWhite) {
+        setTile(row, column, isWhite ? POS_MARKED_WHITE : POS_MARKED_BLACK);
+    }
+    
+    public BoardModel(int size) {
+        gameBoard = new String[size][size];
         
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
                 gameBoard[i][j] = POS_AVAILABLE;
             }
         }
+    }
+    
+    // change the game model to reflect a valid move
+    public boolean makeMove(int newQueenRow, int newQueenColumn,
+                                int arrowRow, int arrowColumn,
+                                int oldQueenRow, int oldQueenColumn) {
+        boolean isValid = true;
+        
+        if (newQueenRow >= gameBoard.length | newQueenColumn >= gameBoard.length) {
+            isValid = false;
+        }
+        else if (!gameBoard[newQueenRow][newQueenColumn].equalsIgnoreCase(POS_AVAILABLE)) {
+            isValid = false;
+        }
+        
+        if (isValid) {
+            gameBoard[newQueenRow][newQueenColumn] = gameBoard[oldQueenRow][oldQueenColumn];
+            gameBoard[oldQueenRow][oldQueenColumn] = POS_AVAILABLE;
+            gameBoard[arrowRow][arrowColumn] = POS_MARKED_ARROW;
+        }
+        
+        return isValid;
     }
 }
--- team-00/src/main/java/cosc322/GameDisplay.java
+++ team-00/src/main/java/cosc322/GameDisplay.java
@@ -1,5 +1,8 @@
 package cosc322;
 
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import javax.swing.JPanel;
 
 /**
@@ -8,6 +11,8 @@ import javax.swing.JPanel;
  */
 public class GameDisplay extends JPanel {
     private static final long serialVersionUID = 1L;
+    
+    private int boardSize;
     
     // define the size of the display and its components
     private int width = 500;
@@ -25,11 +30,70 @@ public class GameDisplay extends JPanel {
     // constructor
     public GameDisplay(BoardModel gameModel) {
         this.gameModel = gameModel;
+        this.boardSize = gameModel.getSize();
         
         initialize();
+        repaint();
     }
     
+    // put the queens in their proper places
     private void initialize() {
-        
+        gameModel.setQueen(0, 3, true);
+        gameModel.setQueen(0, 6, true);
+        gameModel.setQueen(2, 0, true);
+        gameModel.setQueen(2, 9, true);
+        gameModel.setQueen(7, 0, false);
+        gameModel.setQueen(7, 9, false);
+        gameModel.setQueen(9, 3, false);
+        gameModel.setQueen(9, 6, false);
+    }
+    
+    private boolean markPosition(int newQueenRow, int newQueenColumn,
+                              int arrowRow, int arrowColumn,
+                              int oldQueenRow, int oldQueenColumn) {
+        boolean isValid = gameModel.makeMove(newQueenRow, newQueenColumn,
+                                             arrowRow, arrowColumn,
+                                             oldQueenRow, oldQueenColumn);
+        repaint();
+        return isValid;
+    }
+    
+    // this is a mysterious jframe method
+    protected void paintComponent(Graphics gg){
+        Graphics g = (Graphics2D) gg;
+
+        for(int i = 0; i < boardSize + 1; i++){
+            g.drawLine(i * cellDim + offset, offset, i * cellDim + offset, boardSize * cellDim + offset);
+            g.drawLine(offset, i*cellDim + offset, boardSize * cellDim + offset, i*cellDim + offset);					 
+        }
+
+        for(int r = 0; r < boardSize; r++){
+            for(int c = 0; c < boardSize; c++){
+
+                posX = c * cellDim + offset;
+                posY = (9 - r) * cellDim + offset;
+
+                if(gameModel.getTile(r, c).equalsIgnoreCase(BoardModel.POS_AVAILABLE)){
+                        g.clearRect(posX, posY, 49, 49);					
+                }
+
+                if(gameModel.getTile(r, c).equalsIgnoreCase(BoardModel.POS_MARKED_BLACK)){
+                        g.fillOval(posX, posY, 50, 50);
+                } 
+                else if(gameModel.getTile(r, c).equalsIgnoreCase(BoardModel.POS_MARKED_ARROW)) {
+                        g.clearRect(posX + 1, posY + 1, 49, 49);
+                        g.drawLine(posX, posY, posX + 50, posY + 50);
+                        g.drawLine(posX, posY + 50, posX + 50, posY);
+                }
+                else if(gameModel.getTile(r, c).equalsIgnoreCase(BoardModel.POS_MARKED_WHITE)){
+                        g.drawOval(posX, posY, 50, 50);
+                }
+            }
+        }
+    }
+    
+    // another mysterious jframe method!
+    public Dimension getPreferredSize() {
+        return new Dimension(500,500);
     }
 }
